692b23241899dc46a674f3b15dfc3a25171f0159
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-02-02 18:15:49 +0500

    empty component add, cart component , wishies components  add

--- src/Routes/Cart/Cart.js
+++ src/Routes/Cart/Cart.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Empty from '../../components/empty/Empty'
 
 function Cart() {
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="container">
+        <Empty
+          title="Savatda hozircha ma-hsulot yoʻq"
+          url="https://uzum.uz/static/img/shopocat.490a4a1.png"
+          desc="Bosh sahifadagi to’plamlardan boshlang yoki kerakli mahsulotni qidiruv orqali toping"
+          btnTitle="Bosh sahifa"
+          link="/"
+        />
+      </div>
+    </div>
   )
 }
 
--- src/Routes/Wishies/Wishies.js
+++ src/Routes/Wishies/Wishies.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Empty from '../../components/empty/Empty'
 
 function Wishies() {
   return (
-    <div>Wishies</div>
+    <div className='wishies'>
+      <div className="container">
+        <Empty
+          title="Sizga yoqqanini qoʻshing"
+          url="https://uzum.uz/static/img/hearts.cf414be.png"
+          desc="Mahsulotdagi belgisini bosing. Akkauntga kiring va barcha saralanganlar saqlanib qoladi"
+          btnTitle="Akkountga kirish"
+          link="/login"
+        />
+      </div>
+    </div>
   )
 }
 
