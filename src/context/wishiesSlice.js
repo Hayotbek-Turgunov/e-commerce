b6f91b6ab2f94c5f70239f75e5de323f0f9e62e2
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-02-06 13:02:09 +0500

    initial commit

--- src/context/wishiesSlice.js
+++ src/context/wishiesSlice.js
@@ -3,11 +3,12 @@ import { createSlice } from '@reduxjs/toolkit';
 export const wishiesSlice = createSlice({
   name: 'Wishies',
   initialState: {
-    value: []
+    value: JSON.parse(localStorage.getItem("wishes")) || []
   },
   reducers: {
     addToWishies(state, action) {
       state.value = [...state.value, action.payload]
+      localStorage.setItem("wishes", JSON.stringify(state.value))
     },
     removeFromWishies(state, action) {
       state.value = state.value.filter((el) => el._id !== action.payload._id)
